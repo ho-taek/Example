c65f6d3d012cdd52e4951574d04dd5b64744c132
ho-taek <kht072623@naver.com>
2022-10-10 17:58:55 +0900

    UnitTest 계산 연습

---
+++ app/src/main/java/com/example/example/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.example
+
+class Calculator() {
+
+    fun add(a : Int, b: Int) : Int{
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int) : Int{
+        return a - b
+    }
+}
