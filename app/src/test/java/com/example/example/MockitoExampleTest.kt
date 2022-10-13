01971f21ee7198baffe3d62816e43dc0b7bd9739
ho-taek <kht072623@naver.com>
2022-10-13 19:40:21 +0900

    mockito 예외처리

--- app/src/test/java/com/example/example/MockitoExampleTest.kt
+++ app/src/test/java/com/example/example/MockitoExampleTest.kt
@@ -1,11 +1,13 @@
 package com.example.example
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
+import java.lang.IllegalStateException
 
 
 @RunWith(MockitoJUnitRunner::class)
@@ -31,7 +33,27 @@ class MockitoExampleTest{
         val example2 = mock(MockitoExample::class.java)
         lenient().`when`(example2.getId()).thenReturn(100)
         lenient().`when`(example2.getUrl(100)).thenReturn("https://codechacha.com")
+    }
 
+    @Test
+    fun example2(){
+        //anyInt 안에 들어가는 parameter가 상관 없을때
+        val example = mock(MockitoExample::class.java)
+        `when`(example.getUrl(anyInt())).thenReturn("https://codechacha.com")
+
+        assertEquals("https://codechacha.com", example.getUrl(10))
+        //예외 처리
+        `when`(example.getUrl(20)).thenThrow(IllegalStateException("예외 발생"))
+
+        try{
+            example.getUrl(20)
+            fail()
+        }catch (e: IllegalStateException){
+            assertEquals(e.message, "예외 발생")
+        }
+
+        doReturn(30).`when`(example).getId()
+        assertEquals(30, example.getId())
     }
 
 
